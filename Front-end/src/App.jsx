759cc51e5177f665ef3409d12ba97bb500fc9351
Jashwanthbablu18 <jashwanthbablu18@gmail.com>
2025-08-31 18:46:22 +0530

    Updated App.jsx

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,17 +1,17 @@
-// Front-end/src/App.jsx
+// src/App.jsx
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx"
-import SearchResults from "./pages/SearchResults.jsx"
-import BookDetails from "./pages/BookDetails.jsx"
+import BookPage from "./pages/BookPage.jsx"
+import About from "./pages/About.jsx"
 import NotFound from "./pages/NotFound.jsx"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchResults />} />
-      <Route path="/book/:id" element={<BookDetails />} />
+      <Route path="/book/:workId" element={<BookPage />} />
+      <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
