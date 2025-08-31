d09a22a854e24657ce342a289e65e6bbe4fbf4e5
Jashwanthbablu18 <jashwanthbablu18@gmail.com>
2025-08-31 18:09:40 +0530

    Updating App.jsx

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,10 +1,10 @@
 // Front-end/src/App.jsx
 import React from "react"
 import { Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import SearchResults from "./pages/SearchResults"
-import BookDetails from "./pages/BookDetails"
-import NotFound from "./pages/NotFound"
+import HomePage from "./pages/HomePage.jsx"
+import SearchResults from "./pages/SearchResults.jsx"
+import BookDetails from "./pages/BookDetails.jsx"
+import NotFound from "./pages/NotFound.jsx"
 
 export default function App() {
   return (
